aa3b69b6dd9516fb835c37011dd058af9053b010
Brahim Tuijine <86200696+BrahimTuijine@users.noreply.github.com>
2024-12-25 22:34:59 +0100

    Initial Commit

---
+++ SwiftUISPMsAndSDKs/SwiftUISPMsAndSDKsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISPMsAndSDKsApp.swift
+//  SwiftUISPMsAndSDKs
+//
+//  Created by MacBook on 25/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISPMsAndSDKsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
