621b857849c8cdbf50b57bfb19e6384d0050715f
Brahim Tuijine <86200696+BrahimTuijine@users.noreply.github.com>
2024-12-26 12:18:37 +0100

    extract SDWebImageLoader in separate view

--- SwiftUISPMsAndSDKs/SDWebImageScreen.swift
+++ SwiftUISPMsAndSDKs/SDWebImageScreen.swift
@@ -9,12 +9,29 @@ import SwiftUI
 import SDWebImageSwiftUI
 
 
+struct SDWebImageLoader: View {
+    let imageUrl: String
+    var body: some View {
+        WebImage(url: URL(string: imageUrl))
+            
+            .onSuccess { image, data, cacheType in
+                //
+            }
+            .onFailure { error in
+                //
+            }
+            .resizable()
+            .indicator(.activity) // Activity Indicator
+            .transition(.fade(duration: 0.5)) // Fade Transition with duration
+            .scaledToFit()
+            .frame(width: 300, height: 300, alignment: .center)
+    }
+}
+
 //https://picsum.photos/id/237/200/300
 struct SDWebImageScreen: View {
     var body: some View {
-        WebImage(url: URL(string: "https://picsum.photos/id/237/200/300"))
-            .resizable()
-            .frame(width: 300, height: 300)
+        SDWebImageLoader(imageUrl: "https://picsum.photos/id/237/200/300")
     }
 }
 
